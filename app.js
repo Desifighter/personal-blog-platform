84ed90bc976272f5083c41dc034e2d35745eb122
keshav keshari <kg1718129@gmail.com>
2024-01-04 01:51:10 +0530

    second try

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ const _ = require("lodash");
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect(process.env.uri);
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+const PORT = process.env.PORT || 3000;
+connectDB().then(() => {
+  app.listen(PORT||3000, () => {
+    console.log("listening for requests");
+  });
+});
+
+// mongoose.connect(process.env.uri);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -71,8 +88,8 @@ app.get("/posts/:postName", async function(req, res){
 
 }); 
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log("Example app listening on port " + PORT));
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => console.log("Example app listening on port " + PORT));
 
 
 app.get("/about", function (req, res) {
